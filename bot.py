4aa310d3bba7eae82aa4ee3d0dc6af9f60242645
pespolovigor-ai <pespolovigor@gmail.com>
2026-05-22 00:38:04 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageH
 BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
 
 ADMIN_IDS = [5207853162, 5406117718]
-CHANNEL_ID = "-1002376241083"
+CHANNEL_ID = "@sportgagarinmolodezh"
 
 if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN is not set in environment variables")
